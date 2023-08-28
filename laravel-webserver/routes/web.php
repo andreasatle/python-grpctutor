fce702767f289b1a94e4c5b0842c612493ad8282
Andreas Atle <atle.andreas@gmail.com>
2023-08-28 11:29:24 -0500

    Add laravel-webserver

---
+++ laravel-webserver/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+//require 'vendor/autoload.php';
+
+use Illuminate\Support\Facades\Route;
+use Hello\HelloClient;
+use Hello\SayHelloRequest;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+$client = new HelloClient('[::]:50051', [
+    'credentials' => Grpc\ChannelCredentials::createInsecure(),
+]);
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/name/{name}', function ($name) use ($client) {
+    $request = new SayHelloRequest();
+    $request->setName($name);
+    list($response, $status) = $client->SayHello($request)->wait();
+    return $response->getMessage();
+});
